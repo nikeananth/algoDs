980dfc1108bc3145de1aa1079b332092a69648f5
vijayananth <nikeananth@gmail.com>
2022-06-14 18:07:52 -0400

    test Hello checkin

---
+++ AlgoProject/src/com/algo/test/SampleHello.java
@@ -0,0 +1,10 @@
+package com.algo.test;
+
+public class SampleHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello java check");
+	}
+
+}
